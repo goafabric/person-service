c8cb70ff0765094407088bb86dc55c35556d9abf
Andreas Mautsch <amautsch>
2022-07-22 11:11:45 +0200

    hints swapped

--- src/main/java/org/goafabric/personservice/crossfunctional/FlywayNativeConfiguration.java
+++ src/main/java/org/goafabric/personservice/crossfunctional/FlywayNativeConfiguration.java
@@ -33,7 +33,6 @@ import java.util.stream.Stream;
         types = {@TypeHint(types = {org.flywaydb.core.internal.logging.slf4j.Slf4jLogCreator.class, org.flywaydb.core.internal.logging.log4j2.Log4j2LogCreator.class}, access = {TypeAccess.DECLARED_CLASSES, TypeAccess.DECLARED_CONSTRUCTORS, TypeAccess.PUBLIC_METHODS})},
         resources = {@ResourceHint(patterns = {"org/flywaydb/core/internal/version.txt"})}
 )
-//also mind the --initialize-at-build-time inside parent pom
 public class FlywayNativeConfiguration {
 }
 
