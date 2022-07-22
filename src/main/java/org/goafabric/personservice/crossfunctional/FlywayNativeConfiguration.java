aa010615d84f4d8f2127f05f505668609a77f407
Andreas Mautsch <amautsch>
2022-07-22 10:48:15 +0200

    hints swapped

--- src/main/java/org/goafabric/personservice/crossfunctional/FlywayNativeConfiguration.java
+++ src/main/java/org/goafabric/personservice/crossfunctional/FlywayNativeConfiguration.java
@@ -29,7 +29,7 @@ import java.util.stream.Stream;
 
 @Configuration
 @NativeHint(trigger = org.flywaydb.core.Flyway.class,
-        initialization = {@InitializationHint(types = {org.flywaydb.core.internal.util.FeatureDetector.class}, initTime = InitializationTime.BUILD)},
+        initialization = {@InitializationHint(types = {org.flywaydb.core.internal.util.FeatureDetector.class, NativePathLocationScanner.class, org.flywaydb.core.internal.logging.buffered.BufferedLogCreator.class}, initTime = InitializationTime.BUILD)},
         types = {@TypeHint(types = {org.flywaydb.core.internal.logging.slf4j.Slf4jLogCreator.class, org.flywaydb.core.internal.logging.log4j2.Log4j2LogCreator.class}, access = {TypeAccess.DECLARED_CLASSES, TypeAccess.DECLARED_CONSTRUCTORS, TypeAccess.PUBLIC_METHODS})},
         resources = {@ResourceHint(patterns = {"org/flywaydb/core/internal/version.txt"})}
 )
