8e9804fa6f1b869a299048452a97dc0600a38cf1
Andreas Mautsch <amautsch>
2023-02-27 10:59:28 +0100

    HttpInterceptor updated

--- src/main/kotlin/org/goafabric/personservice/adapter/CalleeServiceAdapter.kt
+++ src/main/kotlin/org/goafabric/personservice/adapter/CalleeServiceAdapter.kt
@@ -1,17 +1,17 @@
 package org.goafabric.personservice.adapter
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import org.goafabric.personservice.persistence.multitenancy.TenantInspector
 import org.slf4j.LoggerFactory
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory
-import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
 @Component
 @RegisterReflectionForBinding(Callee::class)
+@CircuitBreaker(name = "CalleeService")
 class CalleeServiceAdapter (
     private val restTemplate: RestTemplate) {
     
@@ -20,13 +20,9 @@ class CalleeServiceAdapter (
     @Value("\${adapter.calleeservice.url}")
     private val url: String? = null
 
-    @Autowired
-    private val cbFactory: CircuitBreakerFactory<*, *>? = null
     fun sayMyName(name: String?): Callee? {
         log.info("Calling CalleService ...")
-        val callee = cbFactory!!.create(this.javaClass.simpleName).run(
-            { restTemplate.getForObject("$url/callees/sayMyName?name={name}", Callee::class.java, name) }
-        ) { throwable: Throwable -> throw NoFallbackAvailableException(throwable.message, throwable) }
+        val callee = restTemplate.getForObject("$url/callees/sayMyName?name={name}", Callee::class.java, name)
         log.info("got: $callee")
         return callee
     }
