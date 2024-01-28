e4c69ea89ab4ba1b7c0688a7378081dfd4e90332
Andreas Mautsch <amautsch>
2024-01-28 10:43:27 +0100

    changes

--- src/test/java/org/goafabric/personservice/controller/PersonControllerIT.java
+++ src/test/java/org/goafabric/personservice/controller/PersonControllerIT.java
@@ -1,11 +1,16 @@
 package org.goafabric.personservice.controller;
 
+import org.goafabric.personservice.adapter.Callee;
+import org.goafabric.personservice.adapter.CalleeServiceAdapter;
 import org.goafabric.personservice.controller.dto.Address;
 import org.goafabric.personservice.controller.dto.Person;
 import org.goafabric.personservice.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.aot.DisabledInAotMode;
 
 import java.util.List;
 
@@ -13,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisabledInAotMode
 class PersonControllerIT {
 
     @Autowired
@@ -20,6 +26,9 @@ class PersonControllerIT {
 
     @Autowired
     private PersonRepository personRepository;
+
+    @MockBean
+    private CalleeServiceAdapter calleeServiceAdapter;
 
     @Test
     public void findById() {
@@ -89,6 +98,12 @@ class PersonControllerIT {
 
 
         personRepository.deleteById(person.id());
+    }
+
+    @Test
+    void sayMyName() {
+        Mockito.when(calleeServiceAdapter.sayMyName("Heisenberg")).thenReturn(new Callee("", "Heisenberg"));
+        assertThat(personController.sayMyName("Heisenberg")).isNotNull();
     }
 
     private Address createAddress(String street) {
