416702777c02dda96022883074657442a15f86f6
Andreas Mautsch <amautsch>
2024-03-03 09:08:36 +0100

    tracing fixed

--- src/main/kotlin/org/goafabric/personservice/adapter/AdapterConfiguration.kt
+++ src/main/kotlin/org/goafabric/personservice/adapter/AdapterConfiguration.kt
@@ -1,5 +1,6 @@
 package org.goafabric.personservice.adapter
 
+import org.goafabric.personservice.extensions.HttpInterceptor
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -39,5 +40,26 @@ class AdapterConfiguration {
             return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(builder.build())).build()
                 .createClient(adapterType)
         }
+    }
+
+    fun <A> createAdapter(
+        adapterType: Class<A>?,
+        builder: RestClient.Builder,
+        url: String?,
+        timeout: Long,
+        maxLifeTime: Long?
+    ): A {
+        val requestFactory = SimpleClientHttpRequestFactory()
+        requestFactory.setConnectTimeout(timeout.toInt())
+        requestFactory.setReadTimeout(timeout.toInt())
+        builder.baseUrl(url!!)
+            .defaultHeaders { httpHeaders: HttpHeaders ->
+                httpHeaders.setBasicAuth("admin", "admin") //for OIDC this would be the jwt
+                httpHeaders.add("X-TenantId", HttpInterceptor.getTenantId())
+                httpHeaders.add("X-OrganizationId", HttpInterceptor.getTenantId())
+            }
+            .requestFactory(requestFactory)
+        return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(builder.build())).build()
+            .createClient(adapterType!!)
     }
 }
