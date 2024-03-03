873ea6fd8c9313a40c30275539b132c9c0ca51e1
Andreas Mautsch <amautsch>
2024-03-03 10:59:37 +0100

    tests added

--- src/main/kotlin/org/goafabric/personservice/controller/PersonController.kt
+++ src/main/kotlin/org/goafabric/personservice/controller/PersonController.kt
@@ -1,10 +1,10 @@
 package org.goafabric.personservice.controller
 
-import org.goafabric.personservice.logic.PersonLogic
 import org.goafabric.personservice.controller.vo.Person
+import org.goafabric.personservice.logic.PersonLogic
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
-import jakarta.validation.Valid
+
 
 @RequestMapping(value = ["/persons"], produces = [MediaType.APPLICATION_JSON_VALUE])
 @RestController
@@ -29,6 +29,11 @@ class PersonController (
     @GetMapping("findByLastName")
     fun findByLastName(@RequestParam("lastName") lastName : String) : List<Person> {
         return personLogic.findByLastName(lastName);
+    }
+
+    @GetMapping("findByStreet")
+    fun findByStreet(street: String?): List<Person> {
+        return personLogic.findByStreet(street)
     }
 
     @PostMapping(value = ["save"], consumes = [MediaType.APPLICATION_JSON_VALUE])
--- src/main/kotlin/org/goafabric/personservice/repository/PersonRepository.kt
+++ src/main/kotlin/org/goafabric/personservice/repository/PersonRepository.kt
@@ -1,14 +1,19 @@
 package org.goafabric.personservice.persistence
 
 import org.goafabric.personservice.repository.entity.PersonEo
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
+
 
 interface PersonRepository : CrudRepository<PersonEo, String> {
     fun findByFirstName(firstName: String): List<PersonEo>
 
     @Query("SELECT p FROM PersonEo p where p.lastName = :lastName")
     fun findByLastName(@Param("lastName") lastName: String): List<PersonEo>
+
+    //@EntityGraph(attributePaths = "address")
+    fun findByAddress_StreetContainsIgnoreCase(street: String?): List<PersonEo>
+
 }
