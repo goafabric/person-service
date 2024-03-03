798ee8fdc750562fe0563fd0a8e40005c37324e8
Andreas Mautsch <amautsch>
2024-03-03 10:55:29 +0100

    tests added

--- src/test/kotlin/org/goafabric/personservice/controller/PersonControllerTest.kt
+++ src/test/kotlin/org/goafabric/personservice/controller/PersonControllerTest.kt
@@ -5,36 +5,35 @@ import org.goafabric.personservice.controller.dto.Address
 import org.goafabric.personservice.controller.dto.Person
 import org.goafabric.personservice.logic.PersonLogic
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
-import java.util.*
+import org.mockito.Mockito.*
 
 internal class PersonControllerTest {
-    private val personLogic: PersonLogic = Mockito.mock<PersonLogic>(PersonLogic::class.java)
+    private val personLogic: PersonLogic = mock<PersonLogic>(PersonLogic::class.java)
     private val personController = PersonController(personLogic)
     
     @Test
     fun getById() {
-        Mockito.`when`(personLogic.getById("0")).thenReturn(createPerson())
+        `when`(personLogic.getById("0")).thenReturn(createPerson())
         assertThat(personController.getById("0").lastName).isEqualTo("Simpson")
     }
 
     @Test
     fun findAll() {
-        Mockito.`when`(personLogic.findAll()).thenReturn(listOf(createPerson()))
+        `when`(personLogic.findAll()).thenReturn(listOf(createPerson()))
         assertThat(personController.findAll()).isNotNull().isNotEmpty
         assertThat(personController.findAll()[0].lastName).isEqualTo("Simpson")
     }
 
     @Test
     fun findByFirstName() {
-        Mockito.`when`(personLogic.findByFirstName("Homer")).thenReturn(listOf(createPerson()))
+        `when`(personLogic.findByFirstName("Homer")).thenReturn(listOf(createPerson()))
         assertThat(personController.findByFirstName("Homer")).isNotNull().isNotEmpty
         assertThat(personController.findByFirstName("Homer")[0].firstName).isEqualTo("Homer")
     }
 
     @Test
     fun findByLastName() {
-        Mockito.`when`(personLogic.findByLastName("Simpson")).thenReturn(listOf(createPerson()))
+        `when`(personLogic.findByLastName("Simpson")).thenReturn(listOf(createPerson()))
         assertThat(personController.findByLastName("Simpson")).isNotNull().isNotEmpty
         assertThat(personController.findByLastName("Simpson")[0].lastName).isEqualTo("Simpson")
     }
@@ -42,7 +41,7 @@ internal class PersonControllerTest {
     @Test
     fun save() {
         assertThat(personController.save(createPerson())).isNull()
-        Mockito.verify(personLogic, Mockito.times(1)).save(createPerson())
+        verify(personLogic, times(1)).save(createPerson())
     }
 
     @Test
