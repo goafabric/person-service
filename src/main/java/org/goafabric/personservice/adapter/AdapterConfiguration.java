026b9b28bc7d40cb47019b38ad80dc84f4db808c
Andreas Mautsch <amautsch>
2024-04-13 09:31:13 +0200

    password removed

--- src/main/java/org/goafabric/personservice/adapter/AdapterConfiguration.java
+++ src/main/java/org/goafabric/personservice/adapter/AdapterConfiguration.java
@@ -9,22 +9,24 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+import java.util.Base64;
+
 @Configuration
 public class AdapterConfiguration {
 
     @Bean
     public CalleeServiceAdapter calleeServiceAdapter(RestClient.Builder builder,
-            @Value("${adapter.calleeservice.url}") String url, @Value("${adapter.timeout}") Long timeout) {
-        return createAdapter(CalleeServiceAdapter.class, builder, url, timeout);
+            @Value("${adapter.calleeservice.url}") String url, @Value("${adapter.timeout}") Long timeout, @Value("${adapter.calleeservice.authentication}") String authentication) {
+        return createAdapter(CalleeServiceAdapter.class, builder, url, timeout, new String(Base64.getDecoder().decode(authentication)));
     }
 
-    public static <A> A createAdapter(Class<A> adapterType, RestClient.Builder builder, String url, Long timeout) {
+    public static <A> A createAdapter(Class<A> adapterType, RestClient.Builder builder, String url, Long timeout, String authentication) {
         var requestFactory = new SimpleClientHttpRequestFactory();
         requestFactory.setConnectTimeout(timeout.intValue());
         requestFactory.setReadTimeout(timeout.intValue());
         builder.baseUrl(url)
                 .requestInterceptor((request, body, execution) -> {
-                    request.getHeaders().setBasicAuth("admin", "admin");
+                    request.getHeaders().setBasicAuth(authentication, authentication);
                     TenantContext.getAdapterHeaderMap().forEach((key, value) -> request.getHeaders().set(key, value));
                     return execution.execute(request, body);
                 })
