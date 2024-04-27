608bb74aa10f59650e7926c13ec09a402b6bb08c
Andreas Mautsch <amautsch>
2024-04-27 17:53:54 +0200

    tenantcontext

--- src/test/java/org/goafabric/personservice/controller/CalleeServiceAdapterMockedIT.java
+++ src/test/java/org/goafabric/personservice/controller/CalleeServiceAdapterMockedIT.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisabledInAotMode
-public class CalleeServiceAdapterMockedIt {
+public class CalleeServiceAdapterMockedIT {
     @Autowired
     private PersonController personController;
 
