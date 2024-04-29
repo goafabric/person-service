56de00d3993679b2d66b1f990a6de888ecc19081
Andreas Mautsch <amautsch>
2024-04-29 12:10:41 +0200

    changes

--- src/main/java/org/goafabric/personservice/persistence/entity/PersonEo.java
+++ src/main/java/org/goafabric/personservice/persistence/entity/PersonEo.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Entity
 @Table(name = "person")
 @EntityListeners(AuditTrailListener.class)
-@Document("#{@httpInterceptor.getPrefix()}person")
+@Document("#{@tenantResolver.getPrefix()}person")
 public class PersonEo {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
