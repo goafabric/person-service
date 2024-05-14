2a475abff726160c83e2dfbb2e158ef4f3c4e99a
Andreas Mautsch <amautsch>
2024-05-14 07:33:23 +0200

    changes

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -1,20 +1,10 @@
 package org.goafabric.personservice;
 
-import io.micrometer.observation.ObservationPredicate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.server.observation.ServerRequestObservationContext;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @SpringBootApplication
 public class Application {
@@ -32,17 +22,4 @@ public class Application {
         };
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, @Value("${security.authentication.enabled:true}") boolean isAuthenticationEnabled, HandlerMappingIntrospector introspector) throws Exception {
-        return isAuthenticationEnabled
-                ? http.authorizeHttpRequests(auth -> auth.requestMatchers(new MvcRequestMatcher(introspector, "/actuator/**")).permitAll().anyRequest().authenticated())
-                .httpBasic(httpBasic -> {}).csrf(AbstractHttpConfigurer::disable).build()
-                : http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).httpBasic(httpBasic -> {}).csrf(AbstractHttpConfigurer::disable).build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() { return NoOpPasswordEncoder.getInstance(); }
-
-    @Bean
-    ObservationPredicate disableHttpServerObservationsFromName() { return (name, context) -> !(name.startsWith("spring.security.") || (context instanceof ServerRequestObservationContext serverContext && (serverContext).getCarrier().getRequestURI().startsWith("/actuator"))); }
 }
