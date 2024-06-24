b9833c641b5fd1ce09fd680a322b4e3ee0efbde8
Andreas Mautsch <amautsch>
2024-06-24 10:02:13 +0200

    upgrades

--- doc/loadtest.js
+++ doc/loadtest.js
@@ -1,15 +1,28 @@
+// https://k6.io/docs/get-started/running-k6/
+
 import http from "k6/http";
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 10, // Number of virtual users
+    vus: 1, // Number of virtual users
     duration: '5s', // Duration of the test
 };
 
+const baseUrl = 'http://localhost:50800';
+
 export default function () {
-  const res = http.get("http://localhost:50800/persons/findAll");
-  check(res, { 'status was 200': (r) => r.status == 200 });
+    checkResponse(http.get(`${baseUrl}/persons/findAll`));
+    checkResponse(http.get(`${baseUrl}/persons/findByFirstName?firstName=Homer`));
+    checkResponse(http.get(`${baseUrl}/persons/findByLastName?lastName=Simpson`));
+    checkResponse(http.get(`${baseUrl}/persons/findByStreet?street=Evergreen%20Terrace`));
 }
 
-// https://k6.io/docs/get-started/running-k6/
-// docker run --rm -i grafana/k6 run - < loadtest.js
+function checkResponse(response) {
+    check(response, {
+        'status is 200': (r) => r.status === 200,
+    });
+    if (response.status !== 200) {
+        console.error(`Unexpected status for request: ${response.status}`);
+        //console.error(response.body)
+    }
+}
