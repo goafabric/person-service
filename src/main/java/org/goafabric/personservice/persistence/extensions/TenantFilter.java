95f2372c46b5ae37b58a9fbd7799e2ed8f09dbce
Andreas Mautsch <amautsch>
2024-06-26 08:46:44 +0200

    schema multi tenancy added

--- src/main/java/org/goafabric/personservice/persistence/extensions/TenantFilter.java
+++ src/main/java/org/goafabric/personservice/persistence/extensions/TenantFilter.java
@@ -3,23 +3,28 @@ package org.goafabric.personservice.persistence.extensions;
 import jakarta.persistence.EntityManager;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.flywaydb.core.Flyway;
 import org.goafabric.personservice.extensions.TenantContext;
 import org.hibernate.Session;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.stereotype.Component;
 
 
 @Aspect
 @Component
-@ImportRuntimeHints(FilterAspect.ApplicationRuntimeHints.class)
-public class FilterAspect {
+@ImportRuntimeHints(TenantFilter.ApplicationRuntimeHints.class)
+public class TenantFilter {
 
     private final EntityManager entityManager;
 
-    public FilterAspect(EntityManager entityManager) {
+    public TenantFilter(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
@@ -31,10 +36,20 @@ public class FilterAspect {
         session.enableFilter("organizationFilter").setParameter("organizationId", TenantContext.getOrganizationId());
     }
 
+    @Configuration
+    static class FlywayConfig {
+        @Bean
+        public FlywayMigrationStrategy flywayMigrationStrategy(@Value("${database.provisioning.goals}") String goals) {
+            return goals.contains("-migrate") ? Flyway::migrate : flyway -> {
+            };
+        }
+    }
+
     static class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
         @Override
         public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-            hints.reflection().registerType(FilterAspect.class, MemberCategory.INVOKE_DECLARED_METHODS);
+            hints.reflection().registerType(TenantFilter.class, MemberCategory.INVOKE_DECLARED_METHODS);
         }
     }
+
 }
