19f05d0de4eb162379cf6636ef138f3612e272a2
Andreas Mautsch <amautsch>
2024-08-16 07:24:26 +0200

    cors

--- src/main/java/org/goafabric/personservice/extensions/HttpInterceptor.java
+++ src/main/java/org/goafabric/personservice/extensions/HttpInterceptor.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.ServerHttpObservationFilter;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -36,6 +37,14 @@ public class HttpInterceptor implements HandlerInterceptor {
         @Override
         public void addInterceptors(InterceptorRegistry registry) {
             registry.addInterceptor(new HttpInterceptor());
+        }
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry
+                    .addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS", "HEAD");
         }
     }
 
