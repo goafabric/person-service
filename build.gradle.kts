085a308218ab038ab698064fb7fa08060ca57639
Andreas Mautsch <amautsch>
2024-09-18 15:26:57 +0200

    java23 upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ java.sourceCompatibility = JavaVersion.VERSION_21
 
 val dockerRegistry = "goafabric"
 val nativeBuilder = "paketobuildpacks/java-native-image:9.5.0"
-val baseImage = "ibm-semeru-runtimes:open-21.0.3_9-jre-focal@sha256:5cb19afa9ee0daeecb7c31be8253fecbbf6b5f6dcfb06883c41f045cb893bcec"
+val baseImage = "eclipse-temurin:22.0.2_9-jre-jammy"
 
 plugins {
 	java
