69995e15ca3381ce50d92f8c92de54bb821f9695
Andreas Mautsch <amautsch>
2024-11-05 09:38:35 +0100

    changes

---
+++ doc/examples/javers/generated/MyHibernateConnectionProvider.java
@@ -0,0 +1,38 @@
+package org.goafabric.personservice.persistence.extensions;
+
+import jakarta.persistence.EntityManager;
+import org.hibernate.Session;
+import org.hibernate.jdbc.Work;
+import org.javers.repository.sql.ConnectionProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Component
+public class MyHibernateConnectionProvider implements ConnectionProvider {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public Connection getConnection() {
+        Session session = entityManager.unwrap(Session.class);
+        GetConnectionWork connectionWork = new GetConnectionWork();
+        session.doWork(connectionWork);
+
+        //TODO that's a dirty hack forced by upgrading to Hibernate 6
+        //this method should accept a connection consumer
+        return connectionWork.theConnection;
+    }
+
+    private class GetConnectionWork implements Work {
+        private Connection theConnection;
+        @Override
+        public void execute(Connection connection) throws SQLException {
+            theConnection = connection;
+        }
+
+    }
+}
