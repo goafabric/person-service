b764c2529e484c221bc02cd10733c62e6f589247
Andreas Mautsch <amautsch>
2024-12-06 11:01:58 +0100

    flyway fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
-	id("org.graalvm.buildtools.native") version "0.10.3"
+	id("org.graalvm.buildtools.native") version "0.10.4"
 
 	id("com.google.cloud.tools.jib") version "3.4.4"
 	id("net.researchgate.release") version "3.0.2"
@@ -28,14 +28,6 @@ repositories {
 	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
-//spring boot 3.4.0 native fix
-dependencyManagement {
-	dependencies {
-		dependency("org.flywaydb:flyway-core:10.10.0")
-		dependency("org.flywaydb:flyway-database-postgresql:10.10.0")
-	}
-}
-
 dependencies {
 	constraints {
 		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
@@ -43,9 +35,6 @@ dependencies {
 		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 		implementation("io.github.resilience4j:resilience4j-spring-boot3:2.2.0")
 		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.0.6")
-
-		implementation("org.flywaydb:flyway-core:10.10.0")
-		implementation("org.flywaydb:flyway-database-postgresql:10.10.0")
 
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.3.0")
 	}
@@ -57,11 +46,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	//monitoring
+	/*
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("io.micrometer:micrometer-tracing-bridge-otel")
 	implementation("io.opentelemetry:opentelemetry-exporter-otlp")
 	implementation("net.ttddyy.observation:datasource-micrometer-spring-boot")
+	
+	 */
 
 	//openapi
 	//spring boot 3.4.0 native fix
