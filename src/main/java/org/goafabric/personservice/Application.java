2729a928bd66250c2b33503136def516747deb98
Andreas Mautsch <amautsch>
2024-12-06 11:14:29 +0100

    flyway fixes

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -10,6 +10,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RegisterReflection(classes = PublishingConfigurationExtension.class, memberCategories = MemberCategory.INVOKE_PUBLIC_METHODS)
 public class Application {
 
     public static void main(String[] args) {
