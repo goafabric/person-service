657356313070e19108ed9ffb00777b585e13f512
Andreas Mautsch <amautsch>
2025-01-21 11:23:25 +0100

    hints added

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RegisterReflection(classes = {io.swagger.v3.core.jackson.mixin.Schema31Mixin.TypeSerializer.class, io.swagger.v3.oas.models.media.JsonSchema.class}, memberCategories = MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
 public class Application {
 
     public static void main(String[] args) {
