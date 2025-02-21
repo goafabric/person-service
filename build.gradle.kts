42537d522ecb883e813068fa22e61d0b5e6945c8
Andreas Mautsch <amautsch@gmx.de>
2025-02-21 01:37:52 +0000

    Update plugin org.springframework.boot to v3.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val baseImage = "ibm-semeru-runtimes:open-21.0.4.1_7-jre-focal@sha256:8b94f8b14f
 
 plugins {
 	jacoco
-	id("org.springframework.boot") version "3.4.2"
+	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.10.5"
 	id("com.google.cloud.tools.jib") version "3.4.4"
