eee51ad11d4a2b2117f97c1392e858bb30eccea9
Andreas Mautsch <amautsch>
2025-02-28 15:05:06 +0100

    api versioning

--- src/main/java/org/goafabric/personservice/base/controller/MyOtherController.java
+++ src/main/java/org/goafabric/personservice/base/controller/MyOtherController.java
@@ -1,7 +1,10 @@
 package org.goafabric.personservice.base.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/v1/other", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MyOtherController {
 }
