0c93589090f36902744eaaff7425314e226d9e16
Andreas Mautsch <amautsch>
2025-03-25 08:31:04 +0100

    java24

--- src/main/java/org/goafabric/personservice/persistence/extensions/DemoDataImporter.java
+++ src/main/java/org/goafabric/personservice/persistence/extensions/DemoDataImporter.java
@@ -66,10 +66,19 @@ public class DemoDataImporter implements CommandLineRunner {
 
     private void insertData() {
         IntStream.range(0, 1).forEach(i -> {
+            applicationContext.getBean(PersonLogic.class).save(new Person(null, null, "Bart", "Simpson"
+                    , List.of(createAddress("Everblue Terrace No. " + i))));
+
             applicationContext.getBean(PersonLogic.class).save(new Person(null, null, "Homer", "Simpson"
                     , List.of(createAddress("Evergreen Terrace No. " + i), createAddress("Springfield Power Plant"))));
 
-            applicationContext.getBean(PersonLogic.class).save(new Person(null, null, "Bart", "Simpson"
+            applicationContext.getBean(PersonLogic.class).save(new Person(null, null, "Lisa", "Simpson"
+                    , List.of(createAddress("Everblue Terrace No. " + i))));
+
+            applicationContext.getBean(PersonLogic.class).save(new Person(null, null, "Marge", "Simpson"
+                    , List.of(createAddress("Everblue Terrace No. " + i))));
+
+            applicationContext.getBean(PersonLogic.class).save(new Person(null, null, "Maggie", "Simpson"
                     , List.of(createAddress("Everblue Terrace No. " + i))));
 
             applicationContext.getBean(PersonLogic.class).save(new Person(null, null, "Monty", "Burns"
