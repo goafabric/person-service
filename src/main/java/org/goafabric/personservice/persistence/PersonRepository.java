e3e52735e60f0b5e90f9d799caf04c9ff243f13e
Andreas Mautsch <amautsch>
2025-03-27 15:58:36 +0100

    changes

--- src/main/java/org/goafabric/personservice/persistence/PersonRepository.java
+++ src/main/java/org/goafabric/personservice/persistence/PersonRepository.java
@@ -4,16 +4,17 @@ import org.goafabric.personservice.persistence.entity.PersonEo;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public interface PersonRepository extends CrudRepository<PersonEo, String>, PagingAndSortingRepository<PersonEo, String> {
+public interface PersonRepository extends CrudRepository<PersonEo, String> {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW) //Workaround for getting the version increased, for JPARepository there is already a working method
     default PersonEo saveAndFlush(PersonEo person) { return save(person); }
+
+    List<PersonEo> findAll(Pageable pageable);
 
     List<PersonEo> findByFirstName(String firstName, Pageable pageable);
 
