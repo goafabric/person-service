22d8953f47bbfbed371db3c0b1868e6b58c9784c
Andreas Mautsch <amautsch>
2025-06-30 08:25:09 +0200

    chang

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -1,9 +1,5 @@
 package org.goafabric.personservice;
 
-import org.hibernate.annotations.DialectOverride;
-import org.hibernate.boot.model.relational.ColumnOrderingStrategyStandard;
-import org.hibernate.boot.models.DialectOverrideAnnotations;
-import org.hibernate.boot.models.annotations.internal.CacheAnnotation;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.annotation.RegisterReflection;
 import org.springframework.boot.CommandLineRunner;
@@ -16,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
-@RegisterReflection(classes = {ColumnOrderingStrategyStandard.class, CacheAnnotation.class, DialectOverride.class, DialectOverrideAnnotations.class,
+@RegisterReflection(classes = {org.hibernate.boot.model.relational.ColumnOrderingStrategyStandard.class, org.hibernate.boot.models.annotations.internal.CacheAnnotation.class, org.hibernate.annotations.DialectOverride.class, org.hibernate.boot.models.DialectOverrideAnnotations.class,
         org.hibernate.validator.internal.util.logging.Log_$logger.class, org.hibernate.engine.internal.VersionLogger_$logger.class, org.hibernate.internal.SessionFactoryRegistryMessageLogger_$logger.class}, memberCategories = {MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.ACCESS_DECLARED_FIELDS})
 public class Application {
 
