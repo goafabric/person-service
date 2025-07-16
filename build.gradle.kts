122d3e95891e1770b5f7d70b62355fc29c736c1d
Andreas Mautsch <amautsch>
2025-07-16 07:58:07 +0200

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,9 +46,9 @@ dependencies {
 
 	//monitoring
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.micrometer:micrometer-registry-prometheus")
 
 	/* //TODO
-	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("io.micrometer:micrometer-tracing-bridge-otel")
 	implementation("io.opentelemetry:opentelemetry-exporter-otlp")
 	implementation("net.ttddyy.observation:datasource-micrometer-spring-boot")
