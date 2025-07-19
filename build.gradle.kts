04df128da54550e0c8402db20344f144ec5057d4
Andreas Mautsch <amautsch@gmx.de>
2025-07-19 01:58:17 +0000

    Update plugin org.graalvm.buildtools.native to v0.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.graalvm.buildtools.native") version "0.10.6"
+	id("org.graalvm.buildtools.native") version "0.11.0"
 	id("com.google.cloud.tools.jib") version "3.4.5"
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "6.2.0.5505"
