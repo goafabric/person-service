4e1e075d5f8f6344f0115c5a95d9184124ae5290
Andreas Mautsch <amautsch>
2025-07-26 09:36:24 +0200

    chang

--- src/main/java/org/goafabric/personservice/persistence/entity/PersonEo.java
+++ src/main/java/org/goafabric/personservice/persistence/entity/PersonEo.java
@@ -3,13 +3,14 @@ package org.goafabric.personservice.persistence.entity;
 import jakarta.persistence.*;
 import org.goafabric.personservice.persistence.extensions.AuditTrailListener;
 import org.hibernate.annotations.TenantId;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Entity
 @Table(name = "person")
 @EntityListeners(AuditTrailListener.class)
-//@Document("#{@httpInterceptor.getTenantPrefix()}person") @org.springframework.data.elasticsearch.annotations.Document(indexName = "#{@httpInterceptor.getTenantPrefix()}person") //TODO
+@Document("#{@httpInterceptor.getTenantPrefix()}person") //@org.springframework.data.elasticsearch.annotations.Document(indexName = "#{@httpInterceptor.getTenantPrefix()}person") //TODO
 public class PersonEo {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
