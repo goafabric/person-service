784ec91bfa267cdf50e6f309849b1d5c1cfb4c8f
Andreas Mautsch <amautsch>
2025-07-26 09:12:24 +0200

    boot 4.0

--- src/main/kotlin/org/goafabric/personservice/Application.kt
+++ src/main/kotlin/org/goafabric/personservice/Application.kt
@@ -1,5 +1,15 @@
 package org.goafabric.personservice
 
+import org.hibernate.annotations.DialectOverride
+import org.hibernate.boot.model.relational.ColumnOrderingStrategyStandard
+import org.hibernate.boot.models.DialectOverrideAnnotations
+import org.hibernate.boot.models.annotations.internal.CacheAnnotation
+import org.hibernate.dialect.type.*
+import org.hibernate.engine.internal.`VersionLogger_$logger`
+import org.hibernate.internal.`SessionFactoryRegistryMessageLogger_$logger`
+import org.hibernate.validator.internal.util.logging.`Log_$logger`
+import org.springframework.aot.hint.MemberCategory
+import org.springframework.aot.hint.annotation.RegisterReflection
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.ExitCodeGenerator
@@ -12,6 +22,12 @@ import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.web.client.RestClient
 
 @SpringBootApplication
+@RegisterReflection(
+    classes = [ColumnOrderingStrategyStandard::class, CacheAnnotation::class, DialectOverride::class, DialectOverrideAnnotations::class, `Log_$logger`::class, `VersionLogger_$logger`::class, `SessionFactoryRegistryMessageLogger_$logger`::class, PostgreSQLInetJdbcType::class, PostgreSQLIntervalSecondJdbcType::class, PostgreSQLStructPGObjectJdbcType::class, PostgreSQLJsonPGObjectJsonbType::class, PostgreSQLJsonArrayPGObjectJsonbJdbcTypeConstructor::class
+    ],
+    memberCategories = [MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.ACCESS_DECLARED_FIELDS]
+)
+
 class Application(@Autowired private val context: ConfigurableApplicationContext):
     CommandLineRunner {
     override fun run(vararg args: String?) {
