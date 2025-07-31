dd37cc6a68bafad8adaff72a4b3419dfd46de3a7
Andreas Mautsch <amautsch>
2025-07-31 15:52:19 +0200

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val javaVersion = "21"
 java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
 
 val dockerRegistry = "goafabric"
-val baseImage = "ibm-semeru-runtimes:open-21.0.8_9-jre@sha256:551139c6639d176c9591c2e2eee16b0092b97a31761c8a9202cf9fffc844d845"
+val baseImage = "ibm-semeru-runtimes:open-21.0.8_9-jre@sha256:551139c6639d176c9591c2e2eee16b0092b97a31761c8a9202cf9fffc844d845" //"ibm-semeru-runtimes:open-${javaVersion}-jre"
 
 plugins {
 	java
