2725304c372c085f4cc3be32c2972964fbb87936
Andreas Mautsch <amautsch>
2025-08-02 07:28:45 +0200

    gradle upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ plugins {
 
 	id("org.cyclonedx.bom") version "2.3.1"
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
+
+	id("org.openrewrite.rewrite") version "7.12.1"
 }
 
 repositories {
