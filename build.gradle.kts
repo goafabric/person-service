7d369c85542cb253b45cd68a0ddae218a4a49d1c
Andreas Mautsch <amautsch@gmx.de>
2025-08-13 01:51:30 +0000

    Update plugin org.openrewrite.rewrite to v7.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 	id("org.cyclonedx.bom") version "2.3.1"
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 
-	id("org.openrewrite.rewrite") version "7.12.1"
+	id("org.openrewrite.rewrite") version "7.13.0"
 }
 
 repositories {
