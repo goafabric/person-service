2dd93aa432208206c0b3a3398e72fb3c9f025f51
Andreas Mautsch <amautsch>
2025-10-17 08:56:57 +0200

    chang

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,13 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "4.0.0-M3"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.graalvm.buildtools.native") version "0.11.1"
+	id("org.graalvm.buildtools.native") version "0.11.2"
 
 	id("com.google.cloud.tools.jib") version "3.4.5"
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "6.3.1.5724"
 
-	id("org.cyclonedx.bom") version "3.0.0"
+	id("org.cyclonedx.bom") version "3.0.1"
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 }
 
