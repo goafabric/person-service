2b4cb74fa9e15607b6e43e5bba234b38c5e1150c
Andreas Mautsch <amautsch@gmx.de>
2025-11-21 01:46:01 +0000

    Update all-updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,12 @@ val baseImage = "ibm-semeru-runtimes:open-jdk-25.0.0_36-jre@sha256:8ae073345116c
 
 plugins {
 	jacoco
-	id("org.springframework.boot") version "3.5.7"
+	id("org.springframework.boot") version "3.5.8"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.11.3"
 	id("com.google.cloud.tools.jib") version "3.5.1"
 	id("net.researchgate.release") version "3.1.0"
-	id("org.sonarqube") version "7.0.1.6134"
+	id("org.sonarqube") version "7.1.0.6387"
 
 	kotlin("jvm") version "2.2.21"
 	kotlin("plugin.spring") version "2.2.21"
