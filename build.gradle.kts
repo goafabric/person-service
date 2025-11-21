178e3623e4348809e9c84714dba43ab63bba55d9
Andreas Mautsch <amautsch>
2025-11-21 12:53:42 +0100

    spring boot upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("net.ttddyy.observation:datasource-micrometer-spring-boot") //TODO
 
 	//openapi
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui") //TODO
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
 
 	//adapter
 	implementation("io.github.resilience4j:resilience4j-spring-boot3")
