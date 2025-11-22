346d4aafa60a9e767ef66c66e2f3b1612c49efd8
Andreas Mautsch <amautsch>
2025-11-22 07:57:19 +0100

    spring boot upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 		implementation("org.mapstruct:mapstruct:1.6.3")
 		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.0")
 		implementation("io.github.resilience4j:resilience4j-spring-boot3:2.3.0")
-		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:2.0.0-RC1")
+		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:2.0.0")
 
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
 	}
@@ -50,10 +50,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-registry-prometheus")
 
-    implementation("net.ttddyy.observation:datasource-micrometer-spring-boot") //TODO
+    implementation("net.ttddyy.observation:datasource-micrometer-spring-boot")
 
 	//openapi
-	//implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
 
 	//adapter
 	implementation("io.github.resilience4j:resilience4j-spring-boot3")
