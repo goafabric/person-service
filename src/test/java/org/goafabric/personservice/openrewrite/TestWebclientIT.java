b5676129db2e7222c7fa2a2767ab173f921dc65b
Andreas Mautsch <amautsch>
2025-11-25 15:30:12 +0100

    chang

---
+++ src/test/java/org/goafabric/personservice/openrewrite/TestWebclientIT.java
@@ -0,0 +1,23 @@
+/*
+package org.goafabric.personservice.openrewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class TestWebclientIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void test() {
+        webTestClient.toString();
+    }
+}
+
+ */
