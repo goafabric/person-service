2a3454ab27a3987f8130d6bd40b7a8bd135e1298
Andreas Mautsch <amautsch>
2025-12-11 08:51:45 +0100

    chang

--- src/main/java/org/goafabric/personservice/adapter/AdapterConfiguration.java
+++ src/main/java/org/goafabric/personservice/adapter/AdapterConfiguration.java
@@ -1,8 +1,10 @@
 package org.goafabric.personservice.adapter;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.goafabric.personservice.extensions.UserContext;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.annotation.RegisterReflection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +20,7 @@ public class AdapterConfiguration {
     @Bean
     public CalleeServiceAdapter calleeServiceAdapter(RestClient.Builder builder,
             @Value("${adapter.calleeservice.url}") String url, @Value("${adapter.timeout}") Long timeout) {
-        return createAdapter(CalleeServiceAdapter.class, builder, url, timeout);
+        return createResilientAdapter(CalleeServiceAdapter.class, builder, url, timeout);
     }
 
     public static <A> A createAdapter(Class<A> adapterType, RestClient.Builder builder, String url, Long timeout) {
@@ -34,6 +36,35 @@ public class AdapterConfiguration {
         return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(builder.build())).build()
                 .createClient(adapterType);
     }
+
+    @Autowired
+    private CircuitBreakerRegistry circuitBreakerRegistry;
+
+    public <A> A createResilientAdapter(Class<A> adapterType, RestClient.Builder builder, String url, Long timeout) {
+        var cb = circuitBreakerRegistry.circuitBreaker("calleeservice");
+
+        var requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(timeout.intValue());
+        requestFactory.setReadTimeout(timeout.intValue());
+        builder.baseUrl(url)
+                .requestInterceptor((request, body, execution) -> {
+                    UserContext.getAdapterHeaderMap().forEach((key, value) -> request.getHeaders().set(key, value));
+                    return execution.execute(request, body);
+                })
+                .requestInterceptor((request, body, execution) ->
+                        {
+                            try {
+                                return cb.executeCallable(() -> execution.execute(request, body));
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                )
+                .requestFactory(requestFactory);
+        return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(builder.build())).build()
+                .createClient(adapterType);
+    }
+
 
 }
 
