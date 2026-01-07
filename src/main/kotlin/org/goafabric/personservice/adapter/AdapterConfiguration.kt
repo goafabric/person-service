b97154b61d848422ab295d88171364369bfc06ea
Andreas Mautsch <amautsch>
2026-01-07 09:13:27 +0100

    chang

--- src/main/kotlin/org/goafabric/personservice/adapter/AdapterConfiguration.kt
+++ src/main/kotlin/org/goafabric/personservice/adapter/AdapterConfiguration.kt
@@ -1,15 +1,19 @@
 package org.goafabric.personservice.adapter
 
 import org.goafabric.personservice.extensions.UserContext
+import org.springframework.aot.hint.MemberCategory
+import org.springframework.aot.hint.annotation.RegisterReflection
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.web.client.ResourceAccessException
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.support.RestClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
 
 @Configuration
+@RegisterReflection(classes = [ResourceAccessException::class], memberCategories = [MemberCategory.INVOKE_DECLARED_METHODS])
 class AdapterConfiguration {
     @Bean
     fun calleeServiceAdapter(
