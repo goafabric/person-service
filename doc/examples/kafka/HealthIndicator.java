06ee74946cdd27115b4a63a8318f56187b8d600d
Andreas Mautsch <amautsch>
2026-01-29 14:54:44 +0100

    tenantresolver fix

--- doc/examples/kafka/HealthIndicator.java
+++ doc/examples/kafka/HealthIndicator.java
@@ -2,8 +2,10 @@
 
 @Override
 public Health health() {
-    try (AdminClient client = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
-        client.describeCluster().nodes().get(1, TimeUnit.SECONDS);
+    var props = new HashMap<>(kafkaAdmin.getConfigurationProperties());
+    props.put("default.api.timeout.ms", "3000");
+    try (AdminClient client = AdminClient.create(props)) {
+        client.describeCluster().clusterId().get();
         return Health.up().build();
     } catch (Exception e) {
         return Health.down(e).build();
