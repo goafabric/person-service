6f6daa7bffd2d66f521da67e56ad142afd322e81
Andreas Mautsch <amautsch>
2026-03-05 07:45:03 +0100

    chang

--- src/test/kotlin/org/goafabric/personservice/persistence/extensions/PersonConsumer.kt
+++ src/test/kotlin/org/goafabric/personservice/persistence/extensions/PersonConsumer.kt
@@ -4,9 +4,7 @@ import org.goafabric.personservice.controller.dto.Address
 import org.goafabric.personservice.controller.dto.Person
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.kafka.annotation.KafkaHandler
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.annotation.RetryableTopic
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.stereotype.Component
 import java.util.concurrent.CountDownLatch
@@ -14,19 +12,18 @@ import java.util.concurrent.CountDownLatch
 //Without EventData we can now simply consume the Objects itself => No more SafeJsonSerializer + Reflection Registration of Entities required
 //Headers are handled by KafkaInterceptor
 @Component
-@KafkaListener(topics = ["person"], groupId = "person")
 //@RetryableTopic(attempts = "3", dltTopicSuffix = ".DLT")
 class PersonConsumer {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
     val latch: CountDownLatch = CountDownLatch(1)
 
-    @KafkaHandler
+    @KafkaListener(topics = ["person"], groupId = "person")
     fun consumePerson(person: Person, @Header("operation") operation: String) {
         log.info("loopback event for person {} {}", person, operation)
         latch.countDown()
     }
 
-    @KafkaHandler
+    @KafkaListener(topics = ["address"], groupId = "address")
     fun consumeAddress(address: Address, @Header("operation") operation: String) {
         log.info("loopback event for address {} {}", address, operation)
         latch.countDown()
