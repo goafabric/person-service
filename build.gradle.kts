4ce42484c8397544fb9529d2782952be127e51dd
Andreas Mautsch <amautsch@gmx.de>
2026-03-18 03:50:37 +0100

    Update all-updates to v2.3.20 (#43)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,10 @@ plugins {
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "7.2.3.7755"
 
-	kotlin("jvm") version "2.3.10"
-	kotlin("plugin.spring") version "2.3.10"
-	kotlin("plugin.jpa") version "2.3.10"
-	kotlin("kapt") version "2.3.10"
+	kotlin("jvm") version "2.3.20"
+	kotlin("plugin.spring") version "2.3.20"
+	kotlin("plugin.jpa") version "2.3.20"
+	kotlin("kapt") version "2.3.20"
 
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 }
