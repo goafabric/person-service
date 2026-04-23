e143a1321e97c1a41b87fa6ee8c312fb1afbf971
Andreas Mautsch <amautsch>
2026-04-23 08:13:07 +0200

    pitest example

---
+++ doc/examples/pitest/build.gradle.kts
@@ -0,0 +1,138 @@
+import org.springframework.boot.gradle.tasks.aot.ProcessTestAot
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
+val version: String by project
+val javaVersion = "25"
+java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
+
+val dockerRegistry = "goafabric"
+val baseImage = "ibm-semeru-runtimes:open-jdk-25.0.2_10-jre-noble@sha256:b02e4cd184d9ece59b01129af1d0a069fa01e4f0f798f0bc4f3ff1a8391ba694"
+
+plugins {
+	java
+	jacoco
+	id("org.springframework.boot") version "4.0.5"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.graalvm.buildtools.native") version "0.11.5"
+
+	id("com.google.cloud.tools.jib") version "3.5.3"
+	id("net.researchgate.release") version "3.1.0"
+	id("org.sonarqube") version "7.2.3.7755"
+
+	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
+	id("info.solidsoft.pitest") version "1.19.0"
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	constraints {
+		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+		implementation("org.mapstruct:mapstruct:1.6.3")
+		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+		implementation("io.github.resilience4j:resilience4j-spring-boot4:2.4.0")
+
+		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
+	}
+}
+
+dependencies {
+	//web
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	//monitoring
+    implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.micrometer:micrometer-registry-prometheus")
+
+	//openapi
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
+
+	//adapter
+    implementation("org.springframework.boot:spring-boot-starter-restclient")
+    implementation("io.github.resilience4j:resilience4j-spring-boot4")
+	implementation("org.springframework.boot:spring-boot-starter-aspectj")
+
+	//persistence
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa") {exclude("org.glassfish.jaxb", "jaxb-runtime")}
+	implementation("com.h2database:h2")
+	implementation("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
+	implementation("org.flywaydb:flyway-database-postgresql")
+
+	implementation("org.springframework.boot:spring-boot-starter-kafka")
+
+	//mongodb + elastic
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	//implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+
+	//code generation
+	implementation("org.mapstruct:mapstruct")
+	annotationProcessor("org.mapstruct:mapstruct-processor")
+
+	//test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
+
+	//devtools
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("com.tngtech.archunit:archunit-junit5")
+
+	//pitest
+	pitest("org.pitest:pitest-junit5-plugin:1.2.3")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	exclude("**/*NRIT*")
+	finalizedBy("jacocoTestReport")
+	testLogging.events("started", "passed", "skipped", "failed")
+}
+tasks.jacocoTestReport { reports {csv.required.set(true); xml.required.set(true) } }
+
+jib {
+	val amd64 = com.google.cloud.tools.jib.gradle.PlatformParameters(); amd64.os = "linux"; amd64.architecture = "amd64"; val arm64 = com.google.cloud.tools.jib.gradle.PlatformParameters(); arm64.os = "linux"; arm64.architecture = "arm64"
+	from.image = baseImage
+	to.image = "${dockerRegistry}/${project.name}:${project.version}"
+	container.jvmFlags = listOf("-Xms256m", "-Xmx256m")
+	from.platforms.set(listOf(amd64, arm64))
+}
+
+interface InjectedExecOps { @get:Inject val execOps: ExecOperations }
+tasks.register("dockerImageNative") { description= "Native Image"; group = "build"; dependsOn("bootBuildImage") }
+tasks.named<BootBuildImage>("bootBuildImage") {
+	val nativeImageName = "${dockerRegistry}/${project.name}-native:${project.version}"
+	imageName.set(nativeImageName)
+	environment.set(mapOf("BP_NATIVE_IMAGE" to "true", "BP_JVM_VERSION" to javaVersion, "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-J-Xmx8000m -march=compatibility"))
+	doLast {
+		project.objects.newInstance<InjectedExecOps>().execOps.exec { commandLine("/bin/sh", "-c", "docker run --rm $nativeImageName -Dspring.context.exit=onRefresh") }
+		project.objects.newInstance<InjectedExecOps>().execOps.exec { commandLine("/bin/sh", "-c", "docker push $nativeImageName") }
+	}
+}
+
+configure<net.researchgate.release.ReleaseExtension> {
+	buildTasks.set(listOf("build", "test", "jib", "dockerImageNative"))
+	tagTemplate.set("v${version}".replace("-SNAPSHOT", ""))
+}
+
+openApi {
+	outputDir.set(file("doc/generated"))
+	customBootRun { args.set(listOf("--server.port=8080")) }
+	tasks.forkedSpringBootRun { dependsOn("compileAotJava", "processAotResources") }
+}
+
+sonar { properties { property("sonar.exclusions", "**/ApplicationBaseRuntimeHints.*") } }
+
+tasks.withType<ProcessTestAot>().configureEach { enabled = false }
+
+pitest {
+	testStrengthThreshold = 60
+	targetClasses.set(listOf("org.goafabric.*"))
+	targetTests.set(listOf("*.*Test"))
+	excludedClasses.add("*.ApplicationBaseRuntimeHints")
+}
