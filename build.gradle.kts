89ee8ba5e1b8505aeb2020ae1e700bfb611aff48
Andreas Mautsch <amautsch>
2026-07-30 08:33:29 +0200

    chang

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,10 +50,13 @@ dependencies {
 	//web
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-restclient")
+
 
 	//monitoring
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-registry-prometheus")
+	implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
 	implementation("net.ttddyy.observation:datasource-micrometer-spring-boot")
 
 	//openapi
@@ -81,9 +84,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
 	//kotlin
-	//implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
-    implementation("org.springframework.boot:spring-boot-starter-restclient")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("tools.jackson.module:jackson-module-kotlin")
 
