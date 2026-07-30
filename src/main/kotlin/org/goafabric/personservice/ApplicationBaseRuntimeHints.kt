75ee7ca782775af022e864594a5f8429c49fbb35
Andreas Mautsch <amautsch>
2026-07-30 09:13:35 +0200

    chang

--- src/main/kotlin/org/goafabric/personservice/ApplicationBaseRuntimeHints.kt
+++ src/main/kotlin/org/goafabric/personservice/ApplicationBaseRuntimeHints.kt
@@ -96,7 +96,9 @@ class ApplicationBaseRuntimeHints : RuntimeHintsRegistrar {
 
         hints.reflection().registerType(
             TypeReference.of("org.hibernate.validator.internal.util.logging.Messages_\$bundle"),
-            Consumer { builder: TypeHint.Builder? -> builder!!.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS) })
+            Consumer { builder: TypeHint.Builder? -> builder!!
+                .withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS).withField("INSTANCE") })
+        
 
         hints.reflection().registerType(
             TypeReference.of("com.google.protobuf.ExtensionRegistry"),
