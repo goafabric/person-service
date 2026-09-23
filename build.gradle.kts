c59430d3a8d471a37fafe0e5beacd5b1cc1ee61d
Andreas Mautsch <amautsch@gmx.de>
2026-09-23 16:39:17 +0000

    Update dependency org.mockito.kotlin:mockito-kotlin to v6.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
 		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.1")
 		implementation("io.github.resilience4j:resilience4j-spring-boot4:2.4.0")
 		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:2.3.0")
-		testImplementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
+		testImplementation("org.mockito.kotlin:mockito-kotlin:6.4.0")
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.5.0")
 	}
 }
